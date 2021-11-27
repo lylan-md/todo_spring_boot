694a4e52d41f27619659ed876eea04d2597b4233
Lilian Breahna <l.breahna@unifun.com>
2021-11-27 19:06:50 +0200

    Fix task deleting

--- src/main/java/com/testSpring/repositories/TasksRepository.java
+++ src/main/java/com/testSpring/repositories/TasksRepository.java
@@ -2,6 +2,7 @@ package com.testSpring.repositories;
 
 import com.testSpring.entities.Task;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +12,6 @@ public interface TasksRepository extends CrudRepository<Task, Integer> {
     List<Task> findByCategoryIdAndUserId(int categoryId, int userId);
     List<Task> findByImportantAndUserId(boolean important, int userId);
     Optional<Task> findByIdAndUserId(int id, int userId);
+    @Transactional
     void deleteByIdAndUserId(int id, int userId);
 }
